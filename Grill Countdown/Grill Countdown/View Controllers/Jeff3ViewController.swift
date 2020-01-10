80d944856bfa563fdd0b35d32027b306915c09dd
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-10 07:33:35 -0800

    jeff done

---
+++ Grill Countdown/Grill Countdown/View Controllers/Jeff3ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  Jeff3ViewController.swift
+//  Grill Countdown
+//
+//  Created by Jorge Alvarez on 1/10/20.
+//  Copyright © 2020 Jorge Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class Jeff3ViewController: UIViewController {
+
+    @IBAction func backToJeff1(_ sender: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
